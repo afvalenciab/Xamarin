fb5977a76b3548bb7e903e41b4d4f54876e3c7e9
Andres Felipe Valencia Benitez <anfevale@bancolombia.com.co>
2017-08-24 23:09:20 -0500

    Se crea linea base del proyecto de Xamarin forms

---
+++ ToDoForms/ToDoForms/ToDoForms/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoForms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(userEntry.Text) || string.IsNullOrEmpty(PassEntry.Text))
+            {
+                resultLabel.Text = "User Name and password is required";
+            }
+            else
+            {
+                resultLabel.Text = "Login Success";
+                await Navigation.PushAsync(new NewItem());
+            }
+        }
+    }
+}
---
+++ ToDoForms/ToDoForms/ToDoForms/NewItem.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoForms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewItem : ContentPage
+    {
+        public NewItem()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
